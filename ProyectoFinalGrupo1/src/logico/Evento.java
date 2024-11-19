da5b6b3c604bbb00ae0d1e44f3f3be93c886725a
fjesp <fjesp@192.168.0.100>
2024-11-19 16:52:41 -0400

    Agregando Getters and Setters de fecha en clase Evento.

--- ProyectoFinalGrupo1/src/logico/Evento.java
+++ ProyectoFinalGrupo1/src/logico/Evento.java
@@ -5,5 +5,14 @@ import java.util.Date;
 public class Evento {
 	
 	private Date fecha;
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
 	
 }
