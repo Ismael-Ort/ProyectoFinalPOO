65b57a693875dbc4e5a7f6ccc14a2218ac2aa6ed
user <user@192.168.75.1>
2024-11-21 17:58:18 -0400

    Creacion de la clase Comision

--- ProyectoFinalGrupo1/src/logico/Comision.java
+++ ProyectoFinalGrupo1/src/logico/Comision.java
@@ -1,5 +1,25 @@
 package logico;
 
+import java.util.ArrayList;
+
+import logico.Jurado;
+//import logico.Trabajo;
+
 public class Comision {
+	
+	private String idcomision;
+	private String area;
+	private Jurado presidente;
+	//private ArrayList<Jurado>jurados;
+	//private ArrayList<Trabajo>trabajos;
+	
+	public Comision(String codigo, String area, Jurado presidente) {
+		super();
+		this.idcomision = idcomision;
+		this.area = area;
+		this.presidente = presidente;
+		//this.jurados=new ArrayList<>();
+		//this.trabajos=new ArrayList<>();
+	}
 
 }
