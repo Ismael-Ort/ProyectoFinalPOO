471d0c40665265e84b9539788a975e3e0ca03769
user <user@192.168.75.1>
2024-11-21 17:37:53 -0400

    Creacion de clase Evento

--- ProyectoFinalGrupo1/src/logico/Evento.java
+++ ProyectoFinalGrupo1/src/logico/Evento.java
@@ -1,17 +1,32 @@
 package logico;
 
+import java.util.ArrayList;
 import java.util.Date;
+
+//import logico.Comision;
+//import logico.Recurso;
 
 public class Evento {
 	
-	private Date fecha;
-
-	public Date getFecha() {
-		return fecha;
-	}
-
-	public void setFecha(Date fecha) {
-		this.fecha = fecha;
+	private String nombre;
+	private String codigo;
+	private String ubicacion;
+	private String fechainicio;
+	private String fechafinal;
+	private int cupo;
+	//private ArrayList<Recurso>recursos;
+	//private ArrayList<Comision>comisiones;
+	
+	public Evento(String nombre, String codigo,String ubicacion, String fechainicio,String fechafinal,int cupo) {
+		super();
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.ubicacion=ubicacion;
+		this.fechainicio = fechainicio;
+		this.fechafinal=fechafinal;
+		this.cupo=cupo;
+		//this.comisiones = new ArrayList<>();
+		//this.recursos = new ArrayList<>();
 	}
 	
 	
