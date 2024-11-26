a2bae6572671e0f2dd88560fe89d02a0b1d35df2
fjesp <fjesp@192.168.0.100>
2024-11-25 22:39:56 -0400

    Creacion de Agregadores y Buscadores, clase GestionEvento

--- ProyectoFinalGrupo1/src/logico/GestionEvento.java
+++ ProyectoFinalGrupo1/src/logico/GestionEvento.java
@@ -2,6 +2,9 @@ package logico;
 
 import java.util.ArrayList;
 
+import logico.Jurado;
+import logico.Participante;
+import logico.TrabajoCientifico;
 import logico.Comision;
 import logico.Evento;
 import logico.GestionEvento;
@@ -170,5 +173,185 @@ public class GestionEvento {
 	public void setCodcomision(int codcomision) {
 		this.codcomision = codcomision;
 	}
+	
+	
+	//Funciones para agregar
+	
+	public void agregarpersonas(Persona persona) {
+		personas.add(persona);
+		
+		if(persona instanceof Participante)
+			codparticipante++;
+		if(persona instanceof Jurado)
+			codjurado++;
+	}
+	
+	
+	public void agregartrabajo(TrabajoCientifico trabajo) {
+		trabajos.add(trabajo);
+		codtrabajo++;
+	}
+	
+	
+	public void agregarevento(Evento evento) {
+		eventos.add(evento);
+		codevento++;
+	}
+	
+	
+	public void agregarrecurso(Recurso recurso) {
+		recursos.add(recurso);
+		codrecurso++;
+	}
+	
+	
+	public void agregarcomisiones(Comision comicion) {
+		comisiones.add(comicion);
+	}
+	
+	
+	//Funciones para buscar
+	
+	public Comision buscacomision(String codigo) {
+		Comision comi = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < comisiones.size() && encontrado == false) {
+			if(comisiones.get(i).getIdcomision().equals(codigo))
+			{
+				encontrado = true;
+				comi = comisiones.get(i);
+			}
+			
+			i++;
+		}
+		
+		return comi;
+	}
+	
+	public Evento buscarEvento(String codigo) {
+		Evento event = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < eventos.size() && encontrado == false) {
+			if(eventos.get(i).getCodigo().equals(codigo))
+			{
+				encontrado = true;
+				event = eventos.get(i);
+			}
+			
+			i++;
+		}
+		
+		return event;
+	}
+	
+	public TrabajoCientifico buscatrabajo(String codigo) {
+		TrabajoCientifico trab = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < trabajos.size() && encontrado == false) {
+			if(trabajos.get(i).getCodigo().equals(codigo))
+			{
+				encontrado = true;
+				trab = trabajos.get(i);
+			}
+			
+			i++;
+		}
+		
+		return trab;
+	}
+	
+	public Participante buscaparticipante(String codigo) {
+		Participante parti = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < personas.size() && encontrado == false) {
+			if(personas.get(i) instanceof Participante)
+			{
+				if(((Participante)personas.get(i)).getCodparticipante().equals(codigo))
+				{
+					encontrado = true;
+					parti = (Participante)personas.get(i);
+				}
+			}
+			
+			i++;
+		}
+		
+		return parti;
+	}
+	
+	public Participante buscaparticipantebycedula(String cedula) {
+		Participante parti = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < personas.size() && encontrado == false) {
+			if(personas.get(i) instanceof Participante)
+			{
+				if(((Participante)personas.get(i)).getCedula().equals(cedula))
+				{
+					encontrado = true;
+					parti = (Participante)personas.get(i);
+				}
+			}
+			
+			i++;
+		}
+		
+		return parti;
+	}
+	
+	public Jurado buscarJurado(String codigo) {
+		Jurado parti = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < personas.size() && encontrado == false) {
+			if(personas.get(i) instanceof Jurado)
+			{
+				if(((Jurado)personas.get(i)).getCodjurado().equals(codigo))
+				{
+					encontrado = true;
+					parti = (Jurado)personas.get(i);
+				}
+			}
+			
+			i++;
+		}
+		
+		return parti;
+	}
+	
+	public Recurso buscarrecurso(String codigo) {
+		Recurso recu = null;
+		boolean encontrado = false;
+		int i = 0;
+		
+		while(i < recursos.size() && encontrado == false) {
+			if(recursos.get(i).getCodigo().equals(codigo))
+			{
+				encontrado = true;
+				recu = recursos.get(i);
+			}
+			
+			i++;
+		}
+		
+		return recu;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
